3348cb4e7457b6c1db8500c6d0181fd4a46ab932
jakubknczny <jakubknczny@gmail.com>
2021-03-06 22:46:53 +0100

    refactor

--- hero.py
+++ hero.py
@@ -1,71 +1,72 @@
-import PySimpleGUI as sg
+import PySimpleGUI as Sg
 
-sg.theme('DarkPurple1')
+Sg.theme('DarkPurple1')
+
+
 # https://pysimplegui.readthedocs.io/en/latest/cookbook/
-class Hero():
-    
+class Hero:
+
     def __init__(self, name="Jagoda, Python Whisperer", health=100, experience=0):
         self.name = name
         self.health = health
         self.experience = experience
 
-    def getName(self):
+    def get_name(self):
         return self.name
 
-    def getHealth(self):
+    def get_health(self):
         return self.health
 
-    def setHealth(self, health):
+    def set_health(self, health):
         self.health = health
-    
-    def getExperience(self):
+
+    def get_experience(self):
         return self.experience
 
-    def setExperience(self, experience):
+    def set_experience(self, experience):
         self.experience = experience
 
 
 def create_main_window():
-    
+    layout = [[Sg.T('Edit Hero')],
+              [Sg.B('Health -1'), Sg.B('Health +1')],
+              [Sg.B('Experience -1'), Sg.B('Experience +1')],
+              [Sg.Text(size=(50, 1), key='-NAME-')],
+              [Sg.T('Health'), Sg.Text(size=(5, 1), justification='r', key='-HEALTH-')],
+              [Sg.T('Experience'), Sg.Text(size=(5, 1), justification='r', key='-EXPERIENCE-')],
+              [Sg.B('Exit')]]
 
-    layout = [[sg.T('Edit Hero')],
-              [sg.B('Health -1'), sg.B('Health +1')],
-              [sg.B('Experience -1'), sg.B('Experience +1')],
-              [sg.Text(size=(50,1), key='-NAME-')],
-              [sg.T('Health') ,sg.Text(size=(5,1), justification='r', key='-HEALTH-')],
-              [sg.T('Experience') ,sg.Text(size=(5,1), justification='r', key='-EXPERIENCE-')],
-              [sg.B('Exit')]]
-
-    return sg.Window('The Most Awesome Hero Editor', layout)
+    return Sg.Window('The Most Awesome Hero Editor', layout)
 
 
 def main():
     window = None
     hero = Hero()
 
-    while True:             # Event Loop
+    while True:  # Event Loop
         if window is None:
-           window = create_main_window().finalize()
-           window.set_min_size((640,480))
-           window['-NAME-'].update(hero.getName())
-           window['-HEALTH-'].update(hero.getHealth())
-           window['-EXPERIENCE-'].update(hero.getExperience())
+            window = create_main_window().finalize()
+            window.set_min_size((640, 480))
+            window['-NAME-'].update(hero.get_name())
+            window['-HEALTH-'].update(hero.get_health())
+            window['-EXPERIENCE-'].update(hero.get_experience())
 
         event, values = window.read()
-        if event in (sg.WIN_CLOSED, 'Exit'):
+        if event in (Sg.WIN_CLOSED, 'Exit'):
             break
         if event == 'Health -1':
-           hero.setHealth(hero.getHealth() - 1)
-           window['-HEALTH-'].update(hero.getHealth())
+            hero.set_health(hero.get_health() - 1)
+            window['-HEALTH-'].update(hero.get_health())
         if event == 'Health +1':
-           hero.setHealth(hero.getHealth() + 1)
-           window['-HEALTH-'].update(hero.getHealth())
+            hero.set_health(hero.get_health() + 1)
+            window['-HEALTH-'].update(hero.get_health())
         if event == 'Experience -1':
-            hero.setExperience(hero.getExperience() - 1)
-            window['-EXPERIENCE-'].update(hero.getExperience())
+            hero.set_experience(hero.get_experience() - 1)
+            window['-EXPERIENCE-'].update(hero.get_experience())
         if event == 'Experience +1':
-            hero.setExperience(hero.getExperience() + 1)
-            window['-EXPERIENCE-'].update(hero.getExperience())
+            hero.set_experience(hero.get_experience() + 1)
+            window['-EXPERIENCE-'].update(hero.get_experience())
     window.close()
+
 
 main()
